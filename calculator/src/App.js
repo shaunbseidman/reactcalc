e6e30adff9a4b91474a1c3f0378a3e195b927955
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-08-14 09:56:19 -0600

    added flex row for buttons

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from './Components/Button'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
-
+        <div className='row'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>/</Button>
+        </div>
+        <div className='row'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>x</Button>
+        </div>
+        <div className='row'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='row'>
+          <Button>.</Button>
+          <Button>0</Button>
+          <Button>=</Button>
+          <Button>-</Button>
+        </div>
       </div>
     );
   }
--- calculator/src/Components/Button.js
+++ calculator/src/Components/Button.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Button.css'
 
-const Button = (props)=>{
-  return(
-    <div>
-      {props.children}
-    </div>
-  )
+const isOperator = val =>{
+  return !isNaN(val) || val === "." || val === '='
+}
+
+const Button = (props) => {
+  return (<div className={`button-wrapper ${
+    isOperator(props.children)
+      ? null
+      : 'operator'}`}>
+    {props.children}
+  </div>)
 }
 
 export default Button
